ffad0b80b77d16f84efa5a3ffd50bc0e9253b788
bz2027 <bryanz2027@student.cis.edu.hk>
2025-02-14 11:42:41 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,30 +7,35 @@
 */
 
 public class Main {
-
-    public static void main(String[] args) {
-
+    
     }
-
+ 
     // 1. add
-public int add(int num1, int num2){
-
-    return (num1 + num2);
+    public int add(int num1, int num2) {
+        return (num1 + num2);
     }
-    // 2. add
-public int add(int num1, int num2, int num3, int num4) {
-        return (num1 + num2 + num3 + num4);
+        // 2. add
+    public int add(int num1, int num2, int num3, int num4) {        
+        return (add(num1, add(num2, add(num3, num4))));
     }
-    // 3. morningGreeting
-
-    // 4. afternoonGreeting
-
-    // 5. triple
-
+        // 3. morningGreeting
+    public String morningGreeting(String name) {
+        System.out.println("早上好"+name+"!");
+        return name;
+    }
+        // 4. afternoonGreeting
+    public String afternoonGreeting(String name) {
+        System.out.println("下午好, "+name+"!");
+        return name;
+    } 
+        // 5. triple
+    public String triple(String word) {
+        String triple = (word + word + word);
+        System.out.println(triple);
+        return triple;
+    }
     // 6. half
 
     // 7. roundPositiveValueToNearestInteger
 
     // 8. roundNegativeValueToNearestInteger
-
-}

d4ccef5ab8b85bf1395cf1d952bfc89f28b9abca
bz2027 <bryanz2027@student.cis.edu.hk>
2025-02-17 21:59:44 +0800

    Lab2.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Bryan Zhu
 *****************************************************************************************************
 */
 
 public class Main {
-    
-    }
  
     // 1. add
     public int add(int num1, int num2) {
@@ -35,7 +33,18 @@ public class Main {
         return triple;
     }
     // 6. half
-
+    public double half(double num5) {
+        System.out.println(num5 / 2);
+        return num5 / 2;
+    }
     // 7. roundPositiveValueToNearestInteger
-
+    public double roundPositiveValueToNearestInteger(double num6) {
+        System.out.println((int)(num6 + 0.5));
+        return num6 + 0.5;
+    }
     // 8. roundNegativeValueToNearestInteger
+    public double roundNegativeValueToNearestInteger(double num7) {
+        System.out.println((int)(num7 - 0.5));
+        return num7 - 0.5;
+    }
+}
